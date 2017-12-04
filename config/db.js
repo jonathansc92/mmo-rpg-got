00dc1a9044256f68a45ad7e6bb4933d3afa2de0c
Jonathan Cruz <jonathansc92@gmail.com>
2017-12-04 11:58:28 -0200

    Configuration driver MongoDB

---
+++ config/db.js
@@ -0,0 +1,23 @@
+// import mongodb
+var mongo = require('mongodb');
+
+var connMongoDB = function(){
+	console.log('Entrou na função de conexão');
+	var db = new mongo.Db(
+		'got', 
+		new mongo.Server(
+			'localhost', // string content address server
+			'27017', // port conection
+			{}
+		),
+		{} // Configuration optionals
+	);
+
+	return db;
+}
+
+module.exports = function(){
+
+	return connMongoDB;
+
+}

e5c29c739e6625bd0a3dc3d1e3d18cada6b15e1b
Jonathan Cruz <jonathansc92@gmail.com>
2017-12-04 11:30:59 -0200

    Validation Form

--- app/controllers/cadastro.js
+++ app/controllers/cadastro.js
@@ -1,3 +1,26 @@
 module.exports.cadastro = function(application, req, res){
-		res.render('cadastro');
+		res.render('cadastro', {validation: {}, dadosForm: {}});
+}
+
+module.exports.cadastrar = function(application, req, res){
+		//res.send('Teste - Vamos cadastrar');
+
+		var dadosForm = req.body;
+
+		req.assert('nome', 'Nome não pode ser vazio').notEmpty();
+		req.assert('usuario', 'Usuário não pode ser vazio').notEmpty();
+		req.assert('senha', 'Senha não pode ser vazio').notEmpty();
+		req.assert('casa', 'Casa deve estar marcada').notEmpty();
+
+		var errors = req.validationErrors();
+
+		if(errors){
+			res.render('cadastro', {validacao: errors, dadosForm: dadosForm});
+			return;
+		}
+
+		res.send('podemos cadastrar')
+
+
+		//res.render('cadastro');
 }
